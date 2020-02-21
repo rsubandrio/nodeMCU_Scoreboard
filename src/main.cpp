f63d41ea5112b6a228939c75ff97168e62b0cb9f
rsubandrio <riopapaika@gmail.com>
2020-02-21 23:52:11 +0800

    Tambah response status dan beberapa kustomisasi

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,6 @@
 #include <Arduino.h>
 #include <SPI.h>
 #include <ESP8266WiFi.h>
-// #include <DNSServer.h>
 #include <ESP8266WebServer.h>
 #include <HJS589.h>
 #include <fonts/ElektronMart6x8.h>
@@ -19,12 +18,12 @@ IPAddress NMIP(255, 255, 255, 0);
 // DNSServer dnsServer;
 ESP8266WebServer server(80);
 
-#define pin_A 16
-#define pin_B 12
-#define pin_sclk 0
-#define pin_clk 14
-#define pin_r 13
-#define pin_noe 15
+// #define pin_A 16
+// #define pin_B 12
+// #define pin_sclk 0
+// #define pin_clk 14
+// #define pin_r 13
+// #define pin_noe 15
 
 #define DISPLAYS_WIDE 1
 #define DISPLAYS_HIGH 1
@@ -53,6 +52,26 @@ void notFound()
 {
   server.send(404, "text/plain", F("Not Found"));
 }
+
+void drawLeftArrow()
+{
+  dmd.drawRect(14, 1, 17, 7, 0, 0); // hapus
+  dmd.drawLine(17, 1, 17, 7);
+  dmd.drawLine(16, 2, 16, 6);
+  dmd.drawLine(15, 3, 15, 5);
+  dmd.drawLine(14, 4, 14, 4);
+}
+
+void drawRightArrow()
+{
+  dmd.drawRect(14, 1, 17, 7, 0, 0); // hapus
+  dmd.drawLine(17, 4, 17, 4);
+  dmd.drawLine(16, 3, 16, 5);
+  dmd.drawLine(15, 2, 15, 6);
+  dmd.drawLine(14, 1, 14, 7);
+}
+
+int responseSend = 0;
 
 void setup() {
   Serial.begin(115200);
@@ -87,6 +106,39 @@ void setup() {
   server.on("/update", [] () {
     dmd.drawText(0, 0, server.arg("l"));
     dmd.drawText(19, 0, server.arg("r"));
+
+    dmd.drawRect(15,0,16,31, 0, 0);
+
+    if (server.arg("s") == "l")
+    {
+      drawLeftArrow();
+    }
+    else if (server.arg("s") == "r")
+    {
+      drawRightArrow();
+    }
+    else
+    {
+      dmd.drawRect(14, 0, 17, 15, 0, 0);
+      dmd.drawRect(15,0,16,31);
+    }
+    
+
+    if (server.arg("ip") == "false")
+    {
+      dmd.drawRect(14, 9, 17, 13, 1, 0);
+    }
+    else if (server.arg("ip") == "true")
+    {
+      dmd.drawRect(14, 9, 17, 13, 1, 1);
+    }
+    else
+    {
+      dmd.drawRect(14, 0, 17, 15, 0, 0);
+      dmd.drawRect(15,0,16,31);
+    }
+    
+
     if (server.hasArg("gameover") && server.arg("gameover").toInt() == 1)
     {
       dmd.clear();
@@ -115,8 +167,10 @@ void setup() {
       dmd.drawText(0, 0, F("00"));
       dmd.drawRect(15,0,16,31);
       dmd.drawText(19, 0, F("00"));
+      responseSend = 0;
     }
-    server.send(200, "application/json", "{\"status\":\"ok\"}");
+    responseSend++;
+    server.send(200, "application/json", "{\"status\":\"ok\", \"number\":\"" + String(responseSend) + "\"}");
   });
 
   server.onNotFound(notFound);
@@ -132,6 +186,5 @@ void setup() {
 }
 
 void loop() {
-  // dnsServer.processNextRequest();
   server.handleClient();
 }
